1251c750ad3c26231ef8115d4815ee5b17e3e5f0
Max Meindl <max_meindl@gmx.de>
2025-07-11 13:41:16 +0200

    Clean reading with BufReader.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,35 +1,20 @@
 use std::{
-    io::Read,
+    io::{BufRead, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
     let peer_addr = stream.peer_addr()?;
     println!("Client {:?} connected", peer_addr);
 
-    let mut buf = [0_u8; 1024];
-    let mut leftover = Vec::new();
+    let buf_read = BufReader::new(stream);
 
-    loop {
-        let num_read_bytes = stream.read(&mut buf[..])?;
-
-        if num_read_bytes == 0 {
-            println!("Client {:?} disconnected", peer_addr);
-            break;
-        }
-
-        leftover.extend_from_slice(&buf[..num_read_bytes]);
-
-        while let Some(pos) = leftover.iter().position(|&b| b == b'\n') {
-            let mut line: Vec<u8> = leftover.drain(..=pos).collect();
-            line.pop();
-            println!(
-                "Read message with {} bytes: '{}'",
-                line.len(),
-                String::from_utf8_lossy(&line)
-            );
-        }
+    for line in buf_read.lines() {
+        let line = line?;
+        println!("Read message with {} bytes: '{}'", line.len(), line);
     }
+
+    println!("Client {:?} disconnected", peer_addr);
 
     Ok(())
 }
@@ -48,7 +33,7 @@ fn main() -> std::io::Result<()> {
         match stream {
             Ok(stream) => handle_connection(stream),
             Err(e) => Err(e),
-        }?
+        }?;
     }
 
     Ok(())
