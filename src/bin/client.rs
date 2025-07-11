753d8cd9bd38ba8d7c77b0fd2d7b92c70ca359da
Max Meindl <max_meindl@gmx.de>
2025-07-11 14:12:52 +0200

    No server response.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,23 +1,10 @@
-use std::{
-    io::{Read, Write},
-    net::TcpStream,
-    str::from_utf8,
-};
+use std::{io::Write, net::TcpStream};
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:12345")?;
 
     stream.write_all(b"ping")?;
     stream.write_all(b"another ping")?;
-
-    // wait for a response
-    let mut buf = [0_u8; 1024];
-    let num_read_bytes = stream.read(&mut buf)?;
-
-    println!(
-        "Server responded with {num_read_bytes} bytes: '{}'",
-        from_utf8(&buf[..num_read_bytes]).unwrap_or("utf8 conversion error")
-    );
 
     Ok(())
 }
